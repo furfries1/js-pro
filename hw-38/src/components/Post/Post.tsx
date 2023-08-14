68df3e54e424f5acbd7a314bf138f5370796d150
Max <furfries@yandex.ru>
2023-08-14 11:48:38 +0300

    refactor Post component

--- hw-38/src/components/Post/Post.tsx
+++ hw-38/src/components/Post/Post.tsx
@@ -3,15 +3,16 @@ import { IPost } from "../../types/types";
 import "./style.css";
 
 const Post = ({ post }: IPost) => {
+  const { date, title, image, text, lesson_num, author } = post;
   return (
     <div className="post">
-      <div className="post-date">{post.date}</div>
-      <div className="post-title">{post.title}</div>
-      <img src={post.image} alt="img" className="post-img" />
-      <div className="post-text"> {post.text} </div>
+      <div className="post-date">{date}</div>
+      <div className="post-title">{title}</div>
+      <img src={image} alt="img" className="post-img" />
+      <div className="post-text"> {text} </div>
       <div className="author-container">
-        <div className="post-lesson">Lesson: {post.lesson_num}</div>
-        <div className="post-author">Author: {post.author}</div>
+        <div className="post-lesson">Lesson: {lesson_num}</div>
+        <div className="post-author">Author: {author}</div>
       </div>
     </div>
   );
